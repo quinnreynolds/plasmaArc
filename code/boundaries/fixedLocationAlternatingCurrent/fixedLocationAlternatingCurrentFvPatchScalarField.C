f5b434c24e0bc9899c0c302bea6fea91ef852877
Quinn Reynolds <quinn.reynolds@gmail.com>
2026-07-28 14:46:08 +0200

    Correct call signature on alternating current BC

--- code/boundaries/fixedLocationAlternatingCurrent/fixedLocationAlternatingCurrentFvPatchScalarField.C
+++ code/boundaries/fixedLocationAlternatingCurrent/fixedLocationAlternatingCurrentFvPatchScalarField.C
@@ -96,6 +96,24 @@ Foam::fixedLocationAlternatingCurrentFvPatchScalarField::
 fixedLocationAlternatingCurrentFvPatchScalarField
 (
     const fixedLocationAlternatingCurrentFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    mixedFvPatchScalarField(ptf, iF),
+    current_(ptf.current_),
+    currentDensity_(ptf.currentDensity_),
+    frequency_(ptf.frequency_),
+    theta_(ptf.theta_),
+    referencePosition_(ptf.referencePosition_)
+{}
+
+
+Foam::fixedLocationAlternatingCurrentFvPatchScalarField::
+fixedLocationAlternatingCurrentFvPatchScalarField
+(
+    const fixedLocationAlternatingCurrentFvPatchScalarField& ptf,
     const DimensionedField<scalar, volMesh>& iF
 )
 :
